cd103f5a2281c85a65ae7a9d62f4c65d559b156a
Vasu <vasudhagangireddy@gmail.com>
2019-06-04 08:03:54 -0500

    Code for first four points

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,6 +16,7 @@ candidates = []
 
 #Dictionary to store the candidates and their votes
 candidateDictionary = {}
+percentVotes = 0
 
 #print("len(candidates):" + str(len(candidates)))
 #opening the election_data.csv
@@ -53,9 +54,11 @@ with open(election_csv,newline ="") as poll:
             if name == val[2]:
                 votesPerCandidate = votesPerCandidate + 1
         
-        #print(name + "........." + str(votesPerCandidate))
-        #candidateDictionary.update(name:votesPerCandidate)
-        candidateDictionary[name] = votesPerCandidate
+        #calculating percentage votes per candidate
+        percentVotes = round((votesPerCandidate * 100)/totalVotes)
+        #candidateDictionary[name] = votesPerCandidate
+        #percentageVotes[name] = percentVotes
+        candidateDictionary[name] = [percentVotes,votesPerCandidate]
 
         votesPerCandidate = 0
 
@@ -64,6 +67,9 @@ with open(election_csv,newline ="") as poll:
     print("-----------------------------")
     print("Total Votes: " + str(totalVotes))
     print("-----------------------------")
+    print(candidateDictionary[name][1])
+    for name,lst in candidateDictionary.items():
+        print(name + ": " +str(float(lst[0]))+"%  ("+str(lst[1])+")")
     
 
     #Writing to the output file
